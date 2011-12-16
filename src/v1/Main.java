f0b0067534cc5815b124ab868c9978d00d254e1f
y0da@live.fr <yoda@localhost.localdomain>
2011-12-16 09:11:19 +0100

    main vidé

--- src/v1/Main.java
+++ src/v1/Main.java
@@ -6,15 +6,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		int entier = 12;
-		String string = "tagada";
-		System.out.println(string);
+		// TODO Auto-generated method stub 
 
-=======
-	    String a = "aaaaaaaa";
->>>>>>> branch 'master' of ssh://git@github.com/yoda1490/OMGZombies
 	}
 
 }
