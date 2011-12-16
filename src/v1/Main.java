9039355186773640b7b31fd5e757b777ce7673bf
y0da@live.fr <yoda@localhost.localdomain>
2011-12-16 09:00:14 +0100

    azertyuiop

--- src/v1/Main.java
+++ src/v1/Main.java
@@ -7,7 +7,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-bonjou   r;
+		int entier = 12;
+		String string = "tagada";
+		System.out.println(string);
+
 	}
 
 }
